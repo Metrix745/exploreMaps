c7170bc4632596a351fcedf7fd88c5f58a52d71f
Nicholas Harrell <36092766+Metrix745@users.noreply.github.com>
2023-04-04 02:40:02 +0000

    Add new files

---
+++ map2.py
@@ -0,0 +1,5 @@
+#import the google maps api and have it process the area of University of Maryland, Baltimore County
+
+#then sotre the buildings on campus in a graph
+
+#then use the shortest path algorithm to find the shortest path between two buildings, for all buildings
